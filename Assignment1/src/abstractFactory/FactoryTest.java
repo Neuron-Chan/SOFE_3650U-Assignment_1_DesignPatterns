1391e6ec3ffb74b4706b0de5452b7adb875c127f
Honsoloberg <117222684+Honsoloberg@users.noreply.github.com>
2023-09-27 18:57:23 -0400

    Update FactoryTest.java

--- Assignment1/src/abstractFactory/FactoryTest.java
+++ Assignment1/src/abstractFactory/FactoryTest.java
@@ -1,12 +1,18 @@
 public class FactoryTest{
   
-  public static void main (stirng[] args){
-  
-    System.out.println("Hello User, today we will make grocery items");
-    FactoryGenerator a = new FactoryGenerator();
-    
-    
-  
-  }
+  public static void main(String args[]) {
+		GroceryProductFactory appleFactory;
+		GroceryProductFactory bananaFactory;
+		FactoryGenerator gen = new FactoryGenerator();
+		
+		
+		appleFactory = gen.getFactory("Apple");
+		
+		bananaFactory = gen.getFactory("Banana");
+		
+		System.out.println("The price for a Apple is: $" + appleFactory.getProductPrice());
+		
+		System.out.println("The price for a Banana is: $" + bananaFactory.getProductPrice());
+	}
 
 }
