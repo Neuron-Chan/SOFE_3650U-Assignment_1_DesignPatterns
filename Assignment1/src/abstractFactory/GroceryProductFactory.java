0c44b478baffa78472bfbd9aa3f52f68b39ce0ee
NathanPerez18 <45292441+NathanPerez18@users.noreply.github.com>
2023-09-27 17:31:31 -0400

    Update GroceryProductFactory.java

--- Assignment1/src/abstractFactory/GroceryProductFactory.java
+++ Assignment1/src/abstractFactory/GroceryProductFactory.java
@@ -1,7 +1,8 @@
 package abstractFactory;
 
 public interface GroceryProductFactory {
-	
+	public Apples createApple():
+	public Banana createBanana();
 	public GroceryProduct createProduct(double productPrice);
 	
 }
