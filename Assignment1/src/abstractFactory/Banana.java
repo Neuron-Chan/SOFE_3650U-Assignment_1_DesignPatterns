8b25f71a406493cfbbcc640b6f9627b64e8b9baa
NathanPerez18 <45292441+NathanPerez18@users.noreply.github.com>
2023-09-27 15:55:28 -0400

    Create Banana.java
    Banan class

---
+++ Assignment1/src/abstractFactory/Banana.java
@@ -0,0 +1,20 @@
+package abstractFactory;
+
+public class Apples implements GroceryProduct {
+	
+	//Instance Variables
+	private double price;
+	
+	//Price Setters & Getters
+	
+	@Override
+	public void setPrice(double newApplePrice) {
+		this.price = newApplePrice;
+	}
+	
+	@Override
+	public double getPrice() {
+		return this.price;
+	}
+	
+}
