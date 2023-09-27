c7646916bf07d25705f4d02218a6c457f9bd636d
Honsoloberg <117222684+Honsoloberg@users.noreply.github.com>
2023-09-27 17:23:22 -0400

    Update BananaFactory.java

--- Assignment1/src/abstractFactory/BananaFactory.java
+++ Assignment1/src/abstractFactory/BananaFactory.java
@@ -1,9 +1,13 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 package abstractFactory;
 
-public class BananaFactory extends AbstractFactory implements GroceryProductFactory {
+public class BananaFactory implements GroceryProductFactory {
 
 	@Override
-	public GroceryProduct createProduct(double productPrice) {
+	public GroceryProduct getProduct() {
 		
 		GroceryProduct a = new Banana();
 		a.setPrice(productPrice);
@@ -11,5 +15,24 @@ public class BananaFactory extends AbstractFactory implements GroceryProductFact
 		
 		return a;
 	}
-	
+
+	public double getPrice(String product){
+		double value;
+		try{
+			File file = new File("datafile.txt");
+			Scanner reader = new Scanner(file);
+			while (true){
+				String price = reader.nextLine();
+				if(price.contains(product) ){
+					int index = price.indexOf(' ');
+					index--;
+					
+				}else if (!reader.hasNextLine()){
+					
+				}
+			}
+		} catch{
+
+		}
+	}
 }
