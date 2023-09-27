d18ae3a36823e4c39ea8f5ddd6ad6eac25dc2583
NathanPerez18 <45292441+NathanPerez18@users.noreply.github.com>
2023-09-27 16:57:10 -0400

    Update FactoryTest.java

--- Assignment1/src/abstractFactory/FactoryTest.java
+++ Assignment1/src/abstractFactory/FactoryTest.java
@@ -4,7 +4,7 @@ public class FactoryTest{
   
     System.out.println("Hello User, today we will make grocery items");
     FactoryGenerator a = new FactoryGenerator();
-    
+    a.getFactory();
   
   }
 
